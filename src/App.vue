e4aa3f67edd27d9ad58cabbcb735b12346da1e2b
Artem Vlakh <vlakhartem@gmail.com>
2022-10-30 20:18:08 +0100

    added simple header w/no resp mode yet

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
     <SiteHeader></SiteHeader>
+    <main>
+
+    </main>
+    <SiteFooter></SiteFooter>
 </template>
 
 <script>
 
 import SiteHeader from './components/SiteHeader.vue';
+import SiteFooter from './components/SiteFooter.vue';
 
 export default {
     components: {
-        SiteHeader
+        SiteHeader,
+        SiteFooter
     }
 }
 
 </script>
+
+<style>
+
+@import '@/assets/css/norm.scss';
+@import '@/assets/css/fonts.scss';
+@import '@/assets/css/main.scss';
+
+</style>
--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,5 +1,26 @@
 <template>
-    <p>Hello</p>
+    <!-- TODO: side block & page overlay to be done  -->
+    <!-- TODO: mobile menu to be done -->
+    <header>
+        <div class="container">
+            <nav id="main-menu">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#about">About me</a></li>
+                    <li><a href="#stack">Skills</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <!-- SECTIONS:
+        1. HOME
+        2. STACK OF TECHNOLOGIES
+        3. ABOUT ME
+        4. MY PROJECTS
+        5. CONTACT
+    -->
 </template>
 
 <script>
@@ -8,7 +29,6 @@
     }
 </script>
 
-<style>
-    @import '@/assets/css/fonts.scss';
-    @import '@/assets/css/norm.scss';
+<style lang="scss">
+    @import '@/assets/css/header.scss';
 </style>
