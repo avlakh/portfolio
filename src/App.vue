b924cbe8b03910c40c2f0f27adf137e9031fbe62
Artem Vlakh <vlakhartem@gmail.com>
2022-10-30 19:30:10 +0100

    added: fonts, created: header comps

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SiteHeader></SiteHeader>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import SiteHeader from './components/SiteHeader.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components: {
+        SiteHeader
+    }
 }
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+</script>
---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>Hello</p>
+</template>
+
+<script>
+    export default {
+        name: 'SiteHeader',
+    }
+</script>
+
+<style>
+    @import '@/assets/css/fonts.scss';
+    @import '@/assets/css/norm.scss';
+</style>
