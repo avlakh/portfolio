4c874e8e9e81c33bb16f7091cf4d132d73734043
Artem Vlakh <vlakhartem@gmail.com>
2022-11-05 17:41:32 +0100

    added footer

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,5 +1,32 @@
+<template>
+    <footer>
+        <div class="container">
+            <h2>Contact</h2>
+            <div class="footer-content">
+                <div class="footer-contact">
+                    <a href="mailto:vlakhartem@gmail.com"><i class="icon icon-envelope-o"></i><span>vlakhartem@gmail.com</span></a>
+                    <a href="tel:+48795080177"><i class="icon icon-phone"></i><span>+48 795 080 177</span></a>
+                </div>
+                <ul class="footer-icons">
+                    <li><a href="https://www.linkedin.com/in/artem-vlakh-407b36183/" target="_blank" title="LinkedIn"><i class="icon icon-linkedin-square"></i></a></li>
+                    <li><a href="https://github.com/avlakh" target="_blank" title="GitHub"><i class="icon icon-github"></i></a></li>
+                    <li><a href="https://www.codewars.com/users/avlakh" target="_blank" title="Codewars"><i class="icon icon-codewars"></i></a></li>
+                    <li><a href="https://codepen.io/avlakh" target="_blank" title="CodePen"><i class="icon icon-codepen"></i></a></li>
+                    <li><a href="https://www.freecodecamp.org/fcc608e09ce-8042-4f7d-bdfc-ce97e48ee189" target="_blank" title="freeCodeCamp"><i class="icon icon-free-code-camp"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+</template>
+
 <script>
 export default {
     name: 'SiteFooter'
 }
 </script>
+
+<style lang="scss">
+
+@import '@/assets/css/footer.scss';
+
+</style>
