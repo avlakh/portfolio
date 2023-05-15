13e98ae27ea10aabb599cbae3a53f407ddbbf494
Artem Vlakh <vlakhartem@gmail.com>
2023-05-15 15:09:25 +0200

    certificate added, old project removed

--- src/components/MyCertificates.vue
+++ src/components/MyCertificates.vue
@@ -1,88 +1,119 @@
 <template>
-    <section id="certificates">
-        <div class="container">
-            <h2>Certificates</h2>
-            <div class="certificates-flex">
-                <div class="card-img">
-                    <a href="https://lms.beetroot.academy/diploma/ckyjz27d0sgnq0824288qos59" target="_blank">
-                        <img src="@/assets/images/certificates/beetroot-certificate.jpg" alt="Beetroot Certificate">
-                    </a>
-                </div>
-                <div class="card-img">
-                    <a href="https://udemy-certificate.s3.amazonaws.com/pdf/UC-a66a0dc3-91b8-4918-88bb-bf57a2d6ff46.pdf" target="_blank">
-                        <img src="@/assets/images/certificates/udemy-react.jpg" alt="Udemy React Project">
-                    </a>
-                </div>
-                <div class="card-img">
-                    <a href="https://www.freecodecamp.org/certification/fcc608e09ce-8042-4f7d-bdfc-ce97e48ee189/javascript-algorithms-and-data-structures" target="_blank">
-                        <img src="@/assets/images/certificates/freecodecamp_js.jpg" alt="Free Code Camp JS">
-                    </a>
-                </div>
-                <div class="card-img">
-                    <a href="https://www.freecodecamp.org/certification/fcc608e09ce-8042-4f7d-bdfc-ce97e48ee189/responsive-web-design" target="_blank">
-                        <img src="@/assets/images/certificates/freecodecamp_rwd.jpg" alt="Free Code Camp RWD">
-                    </a>
-                </div>
-            </div>
-        </div>
-    </section>
+	<section id="certificates">
+		<div class="container">
+			<h2>Certificates</h2>
+			<div class="certificates-flex">
+				<div class="card-img">
+					<a
+						href="https://lms.beetroot.academy/diploma/ckyjz27d0sgnq0824288qos59"
+						target="_blank"
+					>
+						<img
+							src="@/assets/images/certificates/beetroot-certificate.jpg"
+							alt="Beetroot Certificate"
+						/>
+					</a>
+				</div>
+				<div class="card-img">
+					<a
+						href="https://udemy-certificate.s3.amazonaws.com/pdf/UC-a66a0dc3-91b8-4918-88bb-bf57a2d6ff46.pdf"
+						target="_blank"
+					>
+						<img
+							src="@/assets/images/certificates/udemy-react.jpg"
+							alt="Udemy React Project"
+						/>
+					</a>
+				</div>
+				<div class="card-img">
+					<a
+						href="https://www.freecodecamp.org/certification/fcc608e09ce-8042-4f7d-bdfc-ce97e48ee189/javascript-algorithms-and-data-structures"
+						target="_blank"
+					>
+						<img
+							src="@/assets/images/certificates/freecodecamp_js.jpg"
+							alt="Free Code Camp JS"
+						/>
+					</a>
+				</div>
+				<div class="card-img">
+					<a
+						href="https://www.freecodecamp.org/certification/fcc608e09ce-8042-4f7d-bdfc-ce97e48ee189/responsive-web-design"
+						target="_blank"
+					>
+						<img
+							src="@/assets/images/certificates/freecodecamp_rwd.jpg"
+							alt="Free Code Camp RWD"
+						/>
+					</a>
+				</div>
+				<div class="card-img">
+					<a
+						href="https://courses.kevinpowell.co/certificates/cert_5V8cEBNp"
+						target="_blank"
+					>
+						<img
+							src="@/assets/images/certificates/kevin-powell-layouts.jpg"
+							alt="Kevin Powell - Conquering Layouts"
+						/>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
 </template>
 
 <script>
 export default {
-    name: 'MyCertificates',
-}
+	name: 'MyCertificates',
+};
 </script>
 
 <style lang="scss">
-
 @import '@/assets/css/vars.scss';
 
 #certificates {
-    padding: 40px 0;
-    scroll-margin-top: 30px;
-    h2 {
-        margin-bottom: 40px;
-    }
+	padding: 40px 0;
+	scroll-margin-top: 30px;
+	h2 {
+		margin-bottom: 40px;
+	}
 }
 
 .certificates-flex {
-    display: flex;
-    flex-wrap: wrap;
-    flex: 1;
-    gap: 40px;
-    .card-img {
-        flex: 35%;
-        border: 10px solid $secondary-clr;
-        border-radius: 12px;
-        box-shadow: 10px 10px 10px #d1d9e6, -10px -10px 10px #f9f9f9;
-        transition: all 0.2s ease;  
-        &:hover {
-            transform: scale(1.05);
-        }
-        img {
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-        }
-    }
+	display: flex;
+	flex-wrap: wrap;
+	flex: 1;
+	gap: 40px;
+	.card-img {
+		flex: 35%;
+		border: 10px solid $secondary-clr;
+		border-radius: 12px;
+		box-shadow: 10px 10px 10px #d1d9e6, -10px -10px 10px #f9f9f9;
+		transition: all 0.2s ease;
+		&:hover {
+			transform: scale(1.05);
+		}
+		img {
+			width: 100%;
+			height: 100%;
+			object-fit: cover;
+		}
+	}
 }
 
 @media screen and (max-width: 750px) {
-    #certificates {
-        padding: 20px 0;
-    }
+	#certificates {
+		padding: 20px 0;
+	}
 }
 
 @media screen and (max-width: 500px) {
-    .certificates-flex .card-img {
-        flex: 50%;
-        &:hover {
-            transform: none;
-        }
-    }
-    
-    
+	.certificates-flex .card-img {
+		flex: 50%;
+		&:hover {
+			transform: none;
+		}
+	}
 }
-
 </style>
--- src/components/MyProjects.vue
+++ src/components/MyProjects.vue
@@ -1,154 +1,169 @@
 <template>
-    <section id="projects">
-        <div class="container">
-            <h2>Projects</h2>
-            <div class="projects-grid">
-                <div class="projects-grid-item" v-for="item in projects" :key="item">
-                    <div class="img-wrap">
-                        <img :src="require('@/assets/images/projects/'+item.img)" :alt="item.imgAlt">
-                    </div>
-                    <div class="content-overlay">
-                        <div class="content-overlay-links">
-                            <a :href="item.githubUrl" target="_blank">See source code</a>
-                            <a :href="item.url">See project</a>
-                        </div>
-                        <p>{{ item.descr }}</p>
-                    </div>
-                    <a class="content-interactive-overlay" :href="item.url"></a>
-                </div>
-            </div>
-        </div>
-    </section>
+	<section id="projects">
+		<div class="container">
+			<h2>Projects</h2>
+			<div class="projects-grid">
+				<div
+					class="projects-grid-item"
+					v-for="item in projects"
+					:key="item"
+				>
+					<div class="img-wrap">
+						<img
+							:src="
+								require('@/assets/images/projects/' + item.img)
+							"
+							:alt="item.imgAlt"
+						/>
+					</div>
+					<div class="content-overlay">
+						<div class="content-overlay-links">
+							<a :href="item.githubUrl" target="_blank"
+								>See source code</a
+							>
+							<a :href="item.url">See project</a>
+						</div>
+						<p>{{ item.descr }}</p>
+					</div>
+					<a class="content-interactive-overlay" :href="item.url"></a>
+				</div>
+			</div>
+		</div>
+	</section>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from 'axios';
 
 export default {
-    name: 'MyProjects',
-    data(){
-        return {
-            projects: []
-        }
-    },
-    created(){
-        axios
-            .get('data/MyProjects.json')
-            .then(resp =>{
-                this.projects = resp.data;
-            });
-    }
-}
+	name: 'MyProjects',
+	data() {
+		return {
+			projects: [],
+		};
+	},
+	created() {
+		axios.get('data/MyProjects.json').then((resp) => {
+			this.projects = resp.data;
+		});
+	},
+};
 </script>
 
 <style lang="scss" scoped>
 @import '@/assets/css/vars.scss';
 
 #projects {
-    padding: 40px 0;
-    scroll-margin-top: 30px;
-    h2 {
-        margin-bottom: 40px;
-    }
+	padding: 40px 0;
+	scroll-margin-top: 30px;
+	h2 {
+		margin-bottom: 40px;
+	}
 }
 
 .projects-grid {
-    width: 100%;
-    height: 100%;
-    display: grid;
-    grid-template: repeat(auto-fit, minmax(150px, 1fr)) / repeat(auto-fit, minmax(300px, 1fr));
-    gap: 3rem;
+	width: 100%;
+	height: 100%;
+	display: grid;
+	grid-template: repeat(auto-fit, minmax(150px, 1fr)) / repeat(
+			auto-fit,
+			minmax(300px, 1fr)
+		);
+	gap: 3rem;
 }
 
 .projects-grid-item {
-    position: relative;
-    border: 10px solid $secondary-clr;
-    height: 100%;
-    width: 100%;
-    border-radius: 12px;
-    box-shadow: 10px 10px 10px #d1d9e6, -10px -10px 10px #f9f9f9;
-    transition: all 0.2s ease;
-    overflow: hidden;
-    &:hover {
-        transform: scale(1.1);
-    }
-    &:hover .content-overlay {
-        bottom: 0%;
-    }
-    &:hover .content-interactive-overlay {
-        bottom: 0%;
-    }
+	position: relative;
+	border: 10px solid $secondary-clr;
+	height: 100%;
+	width: 100%;
+	border-radius: 12px;
+	box-shadow: 10px 10px 10px #d1d9e6, -10px -10px 10px #f9f9f9;
+	transition: all 0.2s ease;
+	overflow: hidden;
+	&:hover {
+		transform: scale(1.1);
+	}
+	&:hover .content-overlay {
+		bottom: 0%;
+	}
+	&:hover .content-interactive-overlay {
+		bottom: 0%;
+	}
 }
 
 .content-overlay {
-    position: absolute;
-    bottom: -100%;
-    left: 0;
-    height: 100%;
-    width: 100%;
-    padding: 2rem;
-    transition: all 0.2s ease;
-    background-color: rgb(233, 241, 251, 1);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    p {
-        text-align: center;
-    }
+	position: absolute;
+	bottom: -100%;
+	left: 0;
+	height: 100%;
+	width: 100%;
+	padding: 2rem;
+	transition: all 0.2s ease;
+	background-color: rgb(233, 241, 251, 1);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	p {
+		text-align: center;
+	}
 }
 
 .content-overlay-links > a {
-    position: relative;
-    display: block;
-    text-align: center;
-    padding: 5px 15px;
-    margin-bottom: 1rem;
-    transition: all 0.2s ease;
-    border-radius: 12px;
-    background: $btn-clr;
-    box-shadow: 2px 2px 2px #d1d9e6, -2px -2px 2px #f9f9f9;
-    z-index: 120;
-    &:hover {
-        color: $accent-clr;
-        &:active {
-            box-shadow: inset 5px 5px 5px #d1d9e6, inset -5px -5px 5px #f9f9f9;
-        }
-    }
+	position: relative;
+	display: block;
+	text-align: center;
+	padding: 5px 15px;
+	margin-bottom: 1rem;
+	transition: all 0.2s ease;
+	border-radius: 12px;
+	background: $btn-clr;
+	box-shadow: 2px 2px 2px #d1d9e6, -2px -2px 2px #f9f9f9;
+	z-index: 120;
+	&:hover {
+		color: $accent-clr;
+		&:active {
+			box-shadow: inset 5px 5px 5px #d1d9e6, inset -5px -5px 5px #f9f9f9;
+		}
+	}
 }
 
 .content-interactive-overlay {
-    position: absolute;
-    height: 100%;
-    width: 100%;
-    z-index: 100;
+	position: absolute;
+	height: 100%;
+	width: 100%;
+	z-index: 100;
 }
 
 .img-wrap {
-    width: 100%;
-    height: 100%;
-    img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
+	width: 100%;
+	height: 100%;
+	img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
 }
 
-@media screen and (min-width: 700px){
-    .projects-grid {
-        grid-template: repeat(auto-fit, minmax(300px, 1fr)) / repeat(auto-fit, minmax(450px, 1fr));
-    }
+@media screen and (min-width: 700px) {
+	.projects-grid {
+		grid-template: repeat(auto-fit, minmax(300px, 1fr)) / repeat(
+				auto-fit,
+				minmax(450px, 1fr)
+			);
+	}
 }
 
 @media screen and (max-width: 750px) {
-    #projects {
-        padding: 20px 0;
-    }
+	#projects {
+		padding: 20px 0;
+	}
 }
 
 @media screen and (max-width: 500px) {
-    .content-overlay {
-        display: none;
-    }
+	.content-overlay {
+		display: none;
+	}
 }
 </style>
